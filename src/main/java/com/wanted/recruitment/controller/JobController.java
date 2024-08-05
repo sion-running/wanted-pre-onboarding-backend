0108b71762748fffce1dce6bcf8d6953c5cd00bd
sion-running <staythismoment.sion@gmail.com>
2024-08-05 12:45:22 +0900

    JobController에서 응답타입을 공통 모델로 변경

--- src/main/java/com/wanted/recruitment/controller/JobController.java
+++ src/main/java/com/wanted/recruitment/controller/JobController.java
@@ -1,15 +1,11 @@
 package com.wanted.recruitment.controller;
 
+import com.wanted.recruitment.controller.response.BaseResponse;
 import com.wanted.recruitment.controller.response.JobDetailResponse;
 import com.wanted.recruitment.controller.response.JobResponse;
-import com.wanted.recruitment.model.entity.Job;
-import com.wanted.recruitment.repository.JobRepository;
 import com.wanted.recruitment.service.JobService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,16 +13,15 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/job")
 public class JobController {
-    private final JobRepository jobRepository;
     private final JobService jobService;
 
     @GetMapping
-    public List<JobResponse> getJobList() {
-        return jobService.getJobList();
+    public BaseResponse<List<JobResponse>> getJobList() {
+        return BaseResponse.success(jobService.getJobList());
     }
 
     @GetMapping("/{jobId}")
-    public JobDetailResponse getJobDetail(@PathVariable("jobId") Long jobId) {
-        return jobService.getJobDetail(jobId);
+    public BaseResponse<JobDetailResponse> getJobDetail(@PathVariable("jobId") Long jobId) {
+        return BaseResponse.success(jobService.getJobDetail(jobId));
     }
 }
